80cb3e61e82928f2f52a920abaa718a35c9534b6
Muhammad Ridho Amrullah <ridhoamrullah99@gmail.com>
2026-07-23 00:21:11 +0700

    Perbaikan axiosInstance

--- frontend-sbubu/src/axiosInstance.jsx
+++ frontend-sbubu/src/axiosInstance.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
 });
 export default instance;
